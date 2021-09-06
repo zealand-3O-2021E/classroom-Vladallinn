eba806c9a277b3410a0413b69906c995b7c1ac0d
Vladimir Sumilin <vladimirshumilin42@gmail.com>
2021-09-06 21:51:10 +0200

    Don't finish

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -10,6 +10,19 @@ namespace ClassRoom
     {
         static void Main(string[] args)
         {
+            ClassRoom ClassRoom = new ClassRoom();
+            ClassRoom.ClassName = "3Q";
+            ClassRoom.SemesterStart = new DateTime(2021, 08, 28);
+            Student student1 = new Student("Vladimir",3, 10);
+            Student student2 = new Student("Raz", 1, 25);
+            Student student3 = new Student("Arturs", 7, 4);
+            Student student4 = new Student("Zdenek",4, 8);
+            Student student5 = new Student("Natalie", 3, 21);
+
+            Console.WriteLine(ClassRoom);
+            Console.ReadLine();
+
         }
+
     }
 }
---
+++ ClassRoom/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Student
+    {
+        private string _name;
+        private int _birthMonth;
+        private int _birthday;
+
+        public string Name { get => _name; set => _name = value; }
+        public int BirthMonth { get => _birthMonth; set => _birthMonth = value; }
+        public int Birthday { get => _birthday; set => _birthday = value; }
+
+
+        public Student(string name, int birthMonth, int birthday)
+        {
+            _name = name;
+            _birthMonth = birthMonth;
+            _birthday = birthday;
+        }
+
+
+        public string Season()
+        {
+            
+            switch (BirthMonth)
+            { 
+                case 1:
+                case 12:
+                case 2:
+                    return "Winter";
+                case 3:
+                case 4:
+                case 5:
+                    return "Spring";
+                case 6:
+                case 7:
+                case 8:
+                    return "Summer";
+                case 9:
+                case 10:
+                case 11:
+                    return "Autumn";
+                default:
+                    throw new ArgumentException("Unexpected number of month");
+            }
+        }
+    }
+}
